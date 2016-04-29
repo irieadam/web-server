11df13248b8b9dba0457b810197df83d8882527d
irieadam <irieadam5@hotmail.com>
2016-04-29 09:37:03 +0200

    Refactored middleware to separate module

--- server.js
+++ server.js
@@ -3,18 +3,7 @@
 var express = require('express');
 var app = express();
 var PORT = 3000;
-
-// define middleware object for express
-var middleware = {
-    requireAuthentication : function (req, res, next) {
-        console.log('private route hit');
-        next();
-    },
-    logger: function (req, res, next) {
-        console.log('Request: ' + new Date().toString() + ' : '+ req.method + ' ' + req.originalUrl);
-        next();
-    }
-};
+var middleware = require('./middleware.js');
 
 // add application level middle ware for auth and logging
 app.use( middleware.logger);
